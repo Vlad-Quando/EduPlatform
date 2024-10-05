2054d51ec271d531aae38fb7c8dc3f0f45c88dc0
Vlad <vlad.up001@gmail.com>
2024-10-05 19:21:33 +0300

    User adding implemented

--- EduPlatform/users/urls.py
+++ EduPlatform/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from users.views import login_view, profile
+from users.views import login_view, profile, useradd
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login_view, name="login"),
-    path('profile/', profile, name="profile")
+    path('profile/', profile, name="profile"),
+    path('useradd/', useradd, name='useradd')
 ]
--- EduPlatform/users/views.py
+++ EduPlatform/users/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from users.forms import UserForm
-from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth import authenticate, login, get_user_model, logout
 from users.models import User
+from exercises.models import SystemImages
 
 # User = get_user_model()
 
@@ -18,19 +19,54 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 user = User.objects.get(username=username)
-                
-            return render(request, 'users/profile.html')
-        else:
-            form = UserForm()
-            return render(request, "users/login.html")
-    else:
-        return render(request, "users/login.html")
+                return render(request, 'users/profile.html')
+            else:
+                form = UserForm()
+    return render(request, "users/login.html")
+    #     else:
+    #         form = UserForm()
+    #         return render(request, "users/login.html")
+    # else:
+    #     return render(request, "users/login.html")
 
 @login_required(login_url="users:login")
 def profile(request):
     user = request.user
+    if request.GET.get('logout', 'false') == 'true':
+        logout(request)
+        return render(request, "users/login.html")
     data = {
         'request': request,
         'user': user
     }
     return render(request, "users/profile.html", data)
+
+
+def useradd(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password and confirm_password and password == confirm_password:
+            user = User.objects.create_user(username, email, password)
+            user.first_name = firstname
+            user.last_name = lastname
+            user.save()
+
+            data = {
+                'request': request,
+                'user': user,
+                'img_placeholder': SystemImages.objects.get(name="Profile_placeholder")
+            }
+            return render(request, "users/profile.html", data)
+        # print('Username:', username)
+        # print('Firstname:', firstname)
+        # print('Lastname:', lastname)
+        # print('Email:', email)
+        # print('Password:', password)
+        # print('Confirm_password:', confirm_password)
+    return render(request, "users/useradd.html")
